1509e2c0d7703294b0c475e13de8ef067dfde913
soheeoott <soheeoott@gmail.com>
2021-04-20 03:26:30 +0900

    [Add] redux AddNumber store 연결

--- redux/redux-number/src/store.js
+++ redux/redux-number/src/store.js
@@ -1,5 +1,11 @@
 import {createStore} from 'redux';
 
 export default createStore(function(state, action){
-   return state;
+    if(state === undefined){
+        return {number: 0}
+    }
+    if(action.type === 'INCREMENT'){
+        return {...state, number:state.number + action.size}
+    }
+    return state;
 })
