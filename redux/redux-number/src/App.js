2b435fc606cfb27ffe75464fb6836aa80ecab109
soheeoott <soheeoott@gmail.com>
2021-04-21 00:16:39 +0900

    [Add] redux AddNumber 종속성 제거

--- redux/redux-number/src/App.js
+++ redux/redux-number/src/App.js
@@ -10,11 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Root</h1>
-        {/* 이벤트 버블링을 통해서 root 부모에 props 로 전달한 값을 다시 호출 */}
-        <AddNumberRoot onClick={function(size){
-          this.setState({number: this.state.number + size});
-        }.bind(this)}></AddNumberRoot>
-        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+        {/* props 제거 */}
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot>
       </div>
     );
   }
