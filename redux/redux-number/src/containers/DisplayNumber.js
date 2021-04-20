8b8a6aa441fbcb734177bde949e138187fce8b41
soheeoott <soheeoott@gmail.com>
2021-04-21 01:04:05 +0900

    [Add] react-redux DisplayNumber

--- redux/redux-number/src/containers/DisplayNumber.js
+++ redux/redux-number/src/containers/DisplayNumber.js
@@ -1,16 +1,3 @@
-import React, { Component } from 'react';
 import DisplayNumber from '../components/DisplayNumber';
-import store from '../store';
-
-export default class extends Component {
-    state = {number:store.getState().number}
-    constructor(props){
-        super(props);
-        store.subscribe(function(){
-            this.setState({number: store.getState().number});
-        }.bind(this));
-    }
-    render(){
-        return <DisplayNumber number={this.state.number}></DisplayNumber>
-    }
-}
+import {connect} from 'react-redux';
+export default connect()(DisplayNumber);
