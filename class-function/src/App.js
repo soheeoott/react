b071c12771ee08db99d06d6dcf00fc16a4f76250
soheeoott <soheeoott@gmail.com>
2021-04-23 00:30:47 +0900

    [Add] 클래스에서 state 를 이용한 날짜 업데이트

--- class-function/src/App.js
+++ class-function/src/App.js
@@ -28,16 +28,25 @@ function FuncComp(props){
 
 class ClassComp extends React.Component {
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
   render(){
     return (
       <div className="container">
         <h2>Class style component</h2>
         <p>Number: {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+        
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date: (new Date()).toString()});
           }.bind(this)
         }></input>
       </div>
