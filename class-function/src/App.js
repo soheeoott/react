c4820fb7212ff4230c68163b1e4be3e913b0aefb
soheeoott <soheeoott@gmail.com>
2021-04-23 00:39:54 +0900

    [Add] 함수에서 useState 를 이용한 날짜 업데이트

--- class-function/src/App.js
+++ class-function/src/App.js
@@ -14,14 +14,29 @@ function FuncComp(props){
   let numberState = useState(props.initNumber);
   let number = numberState[0];
   let setNumber = numberState[1];
+
+  // 방법1
+  // let dateState = useState((new Date()).toString());
+  // let _date = dateState[0];
+  // let setDate = dateState[1];
+
+  // 방법2
+  let [_date, setDate] = useState((new Date()).toString());
   return (
     <div className="container">
       <h2>Function style component</h2>
       <p>Number: {number}</p>
+      <p>Date: {_date}</p>
       <input type="button" value="random" onClick={
           function(){
             setNumber(Math.random());
-          }}></input>
+          }}>
+      </input>
+      <input type="button" value="date" onClick={
+          function(){
+            setDate((new Date()).toString());
+          }}>
+      </input>
     </div>
   )
 }
@@ -37,18 +52,18 @@ class ClassComp extends React.Component {
         <h2>Class style component</h2>
         <p>Number: {this.state.number}</p>
         <p>Date : {this.state.date}</p>
-
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
           }.bind(this)
-        }></input>
-        
+        }>
+        </input>
         <input type="button" value="date" onClick={
           function(){
             this.setState({date: (new Date()).toString()});
           }.bind(this)
-        }></input>
+        }>
+        </input>
       </div>
     )
   }
