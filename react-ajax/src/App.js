55ef238e8247e515f4ada2c7ae1bfcca995989fa
soheeoott <soheeoott@gmail.com>
2021-04-26 23:58:53 +0900

    [Add] 컴포넌트 분리

--- react-ajax/src/App.js
+++ react-ajax/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>WEB</h1>
-        <nav>
+class Nav extends Component {
+  componentDidMount(){
+  }
+  render(){
+    return (
+      <nav>
           <ul>
             <li><a href="1">HTML</a></li>
             <li><a href="2">CSS</a></li>
             <li><a href="3">JS</a></li>
           </ul>
         </nav>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+        <h1>WEB</h1>
+        <Nav></Nav>
         <article>
           <h2>init</h2>
           Hello, React &amp; Ajax
