c0d7878e84de43b81bd90e5d0bf5cde7d9704967
soheeoott <soheeoott@gmail.com>
2021-04-27 23:55:15 +0900

    [Add] 컴포넌트 생성

--- router/src/index.js
+++ router/src/index.js
@@ -6,6 +6,36 @@ function App(){
 	return (
 		<div>
 			<h1>React Router Dom</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+		</div>
+	)
+}
+
+function Home(){
+	return (
+		<div>
+			<h2>Home</h2>
+      Home-Component Page
+		</div>
+	)
+}
+
+function Topics(){
+	return (
+		<div>
+			<h2>Topics</h2>
+      Topics-Component Page
+		</div>
+	)
+}
+
+function Contact(){
+	return (
+		<div>
+			<h2>Contact</h2>
+      Contact-Component Page
 		</div>
 	)
 }
