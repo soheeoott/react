eb35f934635c1afc21e1d9a253f9a77c1ad106dc
soheeoott <soheeoott@gmail.com>
2021-04-27 23:47:53 +0900

    [Add] 개발환경 설정

---
+++ router/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+function App(){
+	return (
+		<div>
+			<h1>React Router Dom</h1>
+		</div>
+	)
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+reportWebVitals();
